24795a2292839fed2fbd5e06c4e1107ada643e9f
luke <lkry95@gmail.com>
2020-11-14 16:18:32 -0500

    Added function cellname and added to Cell Class' __init__.

---
+++ tester.py
@@ -0,0 +1,18 @@
+Nrows = 5
+Ncols = 5
+
+from string import ascii_uppercase
+
+
+def cellname(col, row):
+    # returns a string translates col 0, row 0 to 'A1'
+    cellname = [
+        ['A1', 'A2', 'A3', 'A4', 'A5'],
+        ['B1', 'B2', 'B3', 'B4', 'B5'],
+        ['C1', 'C2', 'C3', 'C4', 'C5'],
+        ['D1', 'D2', 'D3', 'D4', 'D5'],
+        ['E1', 'E2', 'E3', 'E4', 'E5']
+    ]
+    print()
+    return cellname[col][row]
+cellname(1,3)
